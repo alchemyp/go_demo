382f258631e00dad428c7beebe38c82a71e175b3
HUAWEI <alchemy_info@aliyun.com>
2022-01-18 17:15:12 +0800

    zap日志演示完毕

---
+++ README.md
@@ -0,0 +1,4 @@
+#### 使用方法
+
+1. 先编译完项目 go build main.go
+2. 执行项目 ./main config.json   (各个平台有些许差异)
--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"go_demo/config"
+	"go_demo/logger"
 	"net/http"
+	"os"
+
+	"go.uber.org/zap"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//创建一个router实例
-	router := gin.Default()
+	// load config from config.json
+	if len(os.Args) < 1 {
+		return
+	}
 
-	//路由
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+	if err := config.Init(os.Args[1]); err != nil {
+		panic(err)
+	}
+	// init logger
+	if err := logger.InitLogger(config.Conf.LogConfig); err != nil {
+		fmt.Printf("init logger failed, err:%v\n", err)
+		return
+	}
+
+	gin.SetMode(config.Conf.Mode)
+
+	r := gin.Default()
+	// 注册zap相关中间件
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+
+	r.GET("/hello", func(c *gin.Context) {
+		// 假设你有一些数据需要记录到日志中
+		var (
+			name = "q1mi"
+			age  = 18
+		)
+		// 记录日志并使用zap.Xxx(key, val)记录相关字段
+		zap.L().Debug("this is hello func", zap.String("user", name), zap.Int("age", age))
+
+		c.String(http.StatusOK, "hello liwenzhou.com")
 	})
 
-	//默认监听并在 0.0.0.0:8080 上启动服务
-	router.Run()
+	addr := fmt.Sprintf(":%v", config.Conf.Port)
+	r.Run(addr)
 }
